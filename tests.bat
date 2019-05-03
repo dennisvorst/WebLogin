c38059994ff8eb73aad4f493698b1d50ff433db9
Dennis Vorst <dennis.vorst@ziggo.nl>
2019-05-03 16:58:32 +0200

    initial version

---
+++ tests.bat
@@ -0,0 +1,3 @@
+REM https://phpunit.de/getting-started/phpunit-8.html
+REM .\vendor\bin\phpunit --bootstrap .\vendor\autoload.php --testdox tests
+.\vendor\bin\phpunit --bootstrap .\vendor\autoload.php .\tests\Unit
